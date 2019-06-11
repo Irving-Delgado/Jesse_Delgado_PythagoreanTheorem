a2c73abf1fbc1956381b8ad54b147a604a069fe4
Jesse-delgado <50339137+Jesse-delgado@users.noreply.github.com>
2019-06-11 16:12:19 -0500

    Add files via upload

---
+++ HypotenuseAnswer.java
@@ -0,0 +1,7 @@
+public class HypotenuseAnswer {
+	public static void main(String[] args) {
+		Pythagorean total = new Pythagorean();
+		double output = total.calculateHypotenuse(9,11);
+		System.out.println(output);
+	}
+}
